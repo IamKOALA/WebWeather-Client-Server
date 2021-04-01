6b4c29f382fcb482454d69a5707ffd9f299437f1
IamKOALA <ruslan.nasibullin.00@gmail.com>
2021-04-02 00:58:00 +0300

    Add city partly implemented

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-API_KEY = '007542c018f342fcd5ca67d906e197e7'
-API_URL = 'https://api.openweathermap.org/data/2.5/weather'
+const API_KEY = '007542c018f342fcd5ca67d906e197e7'
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather'
+
+const main_city = document.getElementsByClassName('main_city_cont')[0]
+const add_button = document.forms['search']
+const fav_cities = document.getElementsByClassName('fav_cities')[0]
 
 function request(url) {
     return fetch(url).then(response => {
@@ -20,20 +24,19 @@ function getWeatherByName(city) {
 }
 
 function loadCoordViaApi() {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        updateCity(position.coords.latitude, position.coords.longitude);
-    }, function(exception) {
-        updateCity(0, 0);
+    navigator.geolocation.getCurrentPosition(async function(position) {
+        let weather = await getWeatherByCoords(position.coords.latitude, position.coords.longitude)
+        updateCity(weather, main_city, 'main_city')
+    },  async function(exception) {
+        let weather = await getWeatherByName('Omsk')
+        updateCity(weather, main_city, 'main_city')
         console.warn('No access to geolocation: ' + exception.message)
     });
 }
 
-main_city = document.getElementsByClassName('main_city_cont')[0]
-
-async function updateCity (latitude, longitude) {
-    let weather = await getWeatherByCoords(latitude, longitude)
-
+function updateCity (weather, city, class_reg) {
     let rec_city_name = weather['name']
+    alert(rec_city_name)
     let rec_temp = weather['main']['temp_min']
     let rec_wind_speed = weather['wind']['speed']
     let rec_wind_direct = weather['wind']['deg']
@@ -44,11 +47,11 @@ async function updateCity (latitude, longitude) {
     let rec_longitude = weather['coord']['lon']
     console.log(weather)
 
-    main_city.getElementsByClassName('main_city_name')[0].textContent = rec_city_name
-    main_city.getElementsByClassName('main_city_temp')[0].innerHTML = `${Math.round(rec_temp)}°C`
-    main_city.getElementsByClassName('weather_icon')[0].src = `https://openweathermap.org/img/wn/${weather['weather'][0]['icon']}@4x.png`
+    city.getElementsByClassName(`${class_reg}_name`)[0].textContent = rec_city_name
+    city.getElementsByClassName(`${class_reg}_temp`)[0].innerHTML = `${Math.round(rec_temp)}°C`
+    city.getElementsByClassName('weather_icon')[0].src = `https://openweathermap.org/img/wn/${weather['weather'][0]['icon']}@4x.png`
 
-    const weather_info = main_city.getElementsByClassName('city_info')[0];
+    const weather_info = city.getElementsByClassName('city_info')[0];
     weather_info.getElementsByClassName('wind')[0].getElementsByClassName('weather_data_value')[0].textContent = `${rec_wind_speed} m/s, ${rec_wind_direct} deg`
     weather_info.getElementsByClassName('cloudiness')[0].getElementsByClassName('weather_data_value')[0].textContent = rec_cloud
     weather_info.getElementsByClassName('pressure')[0].getElementsByClassName('weather_data_value')[0].textContent = `${rec_pressure}hpa`
@@ -56,6 +59,25 @@ async function updateCity (latitude, longitude) {
     weather_info.getElementsByClassName('location')[0].getElementsByClassName('weather_data_value')[0].textContent = `[${rec_latitude}, ${rec_longitude}]`
 }
 
+async function addCity(city) {
+    const template = document.getElementById('fav_city_templ')
+    const favCityEl = document.importNode(template.content.firstElementChild, true)
+
+    fav_cities.appendChild(favCityEl)
+
+    let weather = await getWeatherByName(city.value)
+
+    updateCity(weather, city, 'fav_city')
+}
+
 update_geo_button = document.getElementsByClassName('refresh_geopos')[0]
 
 update_geo_button.addEventListener('click', function () {loadCoordViaApi()})
+
+add_button.addEventListener('submit', function (e) {
+    const city = document.getElementsByClassName('search_bar')[0]
+
+    addCity(city)
+
+    e.preventDefault()
+})
